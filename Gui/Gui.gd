8ef1f42f84948051cd058a31698977dda2b82794
Elieder JW <eliederjw@gmail.com>
2020-06-03 02:30:08 -0300

    GUI timer removed

--- Gui/Gui.gd
+++ Gui/Gui.gd
@@ -2,31 +2,13 @@ extends CanvasLayer
 
 onready var life_icon = $Control/TextureRect/HBoxContainer/Life/LifeIcon
 
-var timer_stop = false
-
-var time_start = 0
-var time_now = 0
-
-
 func _ready():
 	life_icon.set_frame(Global.player)
-	#timer 
-	time_start = OS.get_unix_time()
-	set_process(true)
 	#stage label
 	$Control/Stage. text = "Fase " + str(Global.level_number)
 	
 func _process(delta):
-	if timer_stop == false:
-		time_now = OS.get_unix_time()
-		var elapsed = time_now - time_start
-		var minutes = elapsed / 60
-		var seconds = elapsed % 60
-		var str_elapsed = "%02d : %02d" % [minutes, seconds]
-		$Control/clock.text = str_elapsed
-
-func stop_timer():
-	timer_stop = true
+	pass
 
 func update_GUI(lives_left, coins):
 	$Control/TextureRect/HBoxContainer/LifeCount.text = str(lives_left)
